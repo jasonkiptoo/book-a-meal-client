cdba7ae638d73b8439e91d5b5700869a209b41e6
JasonKiptoo <kiptoojason@gmail.com>
2022-12-21 11:07:04 +0300

    aded react-router

--- src/components/NavBarAdmin.jsx
+++ src/components/NavBarAdmin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const NavBarAdmin = () => {
-  return {};
-};
+    return (<h1>Admin NAvbar</h1>);
+
+}
 export default NavBarAdmin;
--- src/components/NavBarUser.jsx
+++ src/components/NavBarUser.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const NavBarUser = () => {
-  return {};
+  return (<h1>USer NAvbar</h1>);
 };
 export default NavBarUser;
