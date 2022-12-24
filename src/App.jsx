9eb07ba2b857c9c2f88e21775a886324c9584fb0
Karen Kanana <kananakaren21@gmail.com>
2022-12-24 11:18:07 +0300

    Edit files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Homepage from "./components/HomePage";
+// import Homepage from "./components/HomePage";
 import LoginForm from "./components/LoginForm/LoginForm.jsx";
-import NavBarUser from "./components/NavBarUser";
+// import NavBarUser from "./components/NavBarUser";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-     <NavBarUser/>
-      <Homepage />
       <LoginForm />
 
     </div>
