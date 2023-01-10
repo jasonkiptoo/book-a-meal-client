ce015b5fb6526cdaf4c6f35cba3555c7e624fd6d
JasonKiptoo <kiptoojason@gmail.com>
2023-01-10 21:25:34 +0300

    c

--- src/components/Home/MealCard.jsx
+++ src/components/Home/MealCard.jsx
@@ -2,35 +2,38 @@ import React from "react";
 import "./MealCard.css";
 function MealCard() {
   return (
-    <div class="container mx-auto mt-4">
-      <div class="row">
-        <div class="cardc col-md-4">
-          <div class="card">
+    <div class="container-fluid bg-trasparent my-4 p-3">
+      <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+        <div class="col">
+          <div class="card h-100 shadow-sm">
             <img
               src="https://media.istockphoto.com/id/1339854693/photo/pumpkin-filled-ravioli-pasta-above-table-scene-on-a-dark-background.jpg?b=1&s=170667a&w=0&k=20&c=jKphpHmNNsYr6BGny3WZ722oG2PKPNVy15JysojMQS8="
-              height={300}
               class="card-img-top"
               alt="..."
             />
-          </div>
-          <div class="card-body">
-            <button className="btn btn-danger btn-block">Main Meals</button>
+            <div class="card-body">
+              <div class="d-grid gap-2 my-4">
+                <a href="#" class="btn btn-danger">
+                  Main Meals
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div class="cardc col-md-4">
-          <div class="card">
+        <div class="col">
+          <div class="card h-100 shadow-sm">
             <img
-              src="https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGRyaW5rfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              height={300}
+              src="https://media.istockphoto.com/id/1352121918/photo/christmas-new-year-dishes-traditional-festive-salad-with-edible-vegetarian-christmas-trees.jpg?b=1&s=170667a&w=0&k=20&c=uo21pUKhc-aS8Q-GSgexVzID4ABX1bEECG8PY1QEeEg="
               class="card-img-top"
               alt="..."
             />
-          </div>
-          <div class="card-body">
-            <button type="button" className="btn btn-danger">
-              Drinks
-            </button>
+            <div class="card-body">
+              <div class="d-grid gap-2 my-4">
+                <a href="#" class="btn btn-danger">
+                  Drinks
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
