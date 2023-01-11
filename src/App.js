c92d9f2610302a840f30366d95b02cf45359ca71
Karen Kanana <kananakaren21@gmail.com>
2023-01-11 23:45:14 +0300

    Import register form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 // import Homepage from "./components/HomePage";
 import LoginForm from "./components/LoginForm/LoginForm.js";
+import Register from "./components/LoginForm/RegisterForm.js";
 // import NavBarUser from "./components/NavBarUser";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home.js";
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <LoginForm />
+      {/* <Register /> */}
       <Home />
 
     </div>
