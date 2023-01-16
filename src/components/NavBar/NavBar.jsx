80cccb9ff46560d2fac8ecd139005fd7a9641021
JasonKiptoo <kiptoojason@gmail.com>
2023-01-16 13:11:02 +0300

    updated navbar user name

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ const NavigationBar = () => {
   return (
     <nav className="navbar navbar-custom navbar-expand-sm navbar-dark  py-2 sticky-top navigation">
       <div className="container px-lg-2">
+        {/* <div  className="container1"> */}
         {/* <Link className="navbar-brand mb-2 ms-auto mb-2 mb-lg-0" to="/dashboard">
           GrubHub
         </Link> */}
@@ -41,69 +42,73 @@ const NavigationBar = () => {
                 Order History
               </Link>
             </li>
-
           </ul>
         </div>
       </div>
 
-      {/* <li className="navbar-nav ml-auto">
-
-        <i className="nav-link ">Hello Jayson</i>
-        <i className="nav-link bi bi-instagram"></i>
-        <i className="nav-link bi bi-facebook"> </i>
-        <i className="nav-link bi bi-twitter"> </i>
-
-        <button className="btn bg-light">Logout</button>
-      </li> */}
+      <div className="container-user">Hello Jayson </div>
       <div
-          className="social-container"
+        className="social-container"
+        style={{
+          backgroundColor: "transparent",
+          display: "flex",
+          justifyContent: "flex-end",
+          marginRight: "4rem",
+          marginTop: "1rem",
+        }}
+      >
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
           style={{
-            backgroundColor: "transparent",
-            display: "flex",
-            justifyContent: "flex-end",
-            marginRight: "4rem",
-            marginTop: "1rem",
+            color: "white",
+            marginRight: "1rem",
           }}
         >
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              color: "white",
-              marginRight: "1rem",
-            }}
-          >
-            {/* <FontAwesomeIcon icon={faTwitter} size="2x" /> */}
-            <FaTwitter size="30px" />
-          </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              color: "white",
-              marginRight: "1rem",
-            }}
-          >
-            {/* <FontAwesomeIcon icon={faInstagram} size="2x" /> */}
-            <FaInstagram size="30px" />
-          </a>
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              color: "white",
-            }}
-          >
-            {/* <FontAwesomeIcon icon={faFacebook} size="2x" /> */}
-            <FaFacebook size="30px" />
-          </a>
-          <button className="btn bg-light" style={{width: "100px", color: "#990F02", backgroundColor: "white", marginLeft: "1rem", marginTop: "-5px", fontSize: "16px", fontWeight: "bold"}}>LOGOUT</button>
-        </div>
+          {/* <FontAwesomeIcon icon={faTwitter} size="2x" /> */}
+          <FaTwitter size="30px" />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            color: "white",
+            marginRight: "1rem",
+          }}
+        >
+          {/* <FontAwesomeIcon icon={faInstagram} size="2x" /> */}
+          <FaInstagram size="30px" />
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            color: "white",
+          }}
+        >
+          {/* <FontAwesomeIcon icon={faFacebook} size="2x" /> */}
+          <FaFacebook size="30px" />
+        </a>
+        <button
+          className="btn bg-light"
+          style={{
+            width: "100px",
+            color: "#990F02",
+            backgroundColor: "white",
+            marginLeft: "1rem",
+            marginTop: "-5px",
+            fontSize: "16px",
+            fontWeight: "bold",
+          }}
+        >
+          LOGOUT
+        </button>
+      </div>
+      {/* </div> */}
     </nav>
-    
   );
 };
 
