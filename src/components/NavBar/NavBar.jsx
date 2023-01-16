7e7d9b4854f0251e48cb0721bec573276e0f5837
Karen Kanana <kananakaren21@gmail.com>
2023-01-16 14:25:55 +0300

    create navigation bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,99 +1,85 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaInstagram, FaFacebook, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaFacebook, FaTwitter,  } from "react-icons/fa";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const NavigationBar = () => {
+const Navbar = ({ user, activePage }) => {
   return (
-    
-    <nav className="navbar navbar-custom navbar-expand-sm navbar-dark  py-2 sticky-top navigation" >
-      <div className="container px-lg-2">
-        {/* <div  className="container1"> */}
-        {/* <Link className="navbar-brand mb-2 ms-auto mb-2 mb-lg-0" to="/dashboard">
-          GrubHub
-        </Link> */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <nav style={{ backgroundColor: "#990F02", display: "flex", flexDirection: "row", justifyContent: "space-around", height: "70px"}}>
+      <div className="nav-left" style={{
+        display: "flex",
+        alignItems: "center",
+        marginLeft: "-5rem",
+      }}>
+        <Link
+          to="/menu"
+          style={{ color: activePage === 'menu' ? 'black' : 'white', textDecoration: "none", padding: "20px"}}
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                className="nav-link active"
-                aria-current="page"
-                to="/assessments"
-              >
-                Menu
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/my-orders">
-                My Orders
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/order-history">
-                Order History
-              </Link>
-            </li>
-          </ul>
-        </div>
+          Menu
+        </Link>
+        <Link
+          to="/my-orders"
+          style={{ color: activePage === 'my-orders' ? 'black' : 'white', textDecoration: "none", padding: "20px" }}
+        >
+          My Orders
+        </Link>
+        <Link
+          to="/order-history"
+          style={{ color: activePage === 'order-history' ? 'black' : 'white', textDecoration: "none", padding: "20px" }}
+        >
+          Order History
+        </Link>
       </div>
-
-      <div className="container-user">Hello Jayson </div>
-      <div
-        className="social-container"
-        style={{
-          backgroundColor: "transparent",
-          display: "flex",
-          justifyContent: "flex-end",
-          marginRight: "4rem",
-          marginTop: "1rem",
-        }}
-      >
+      <div className="nav-center" style={{
+        display: "flex",
+        alignItems: "center",
+        color: "white",
+        marginLeft: "-7rem",
+      }}>
+        Hello Jayson {user} &#128522;
+      </div>
+      <div className="nav-right" style={{
+        display: "flex",
+        alignItems: "center",
+        marginRight: "-7rem",
+      }}>
+        <div className="social-icons">
         <a
-          href="https://twitter.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            color: "white",
-            marginRight: "1rem",
-          }}
-        >
-          {/* <FontAwesomeIcon icon={faTwitter} size="2x" /> */}
-          <FaTwitter size="30px" />
-        </a>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            color: "white",
-            marginRight: "1rem",
-          }}
-        >
-          {/* <FontAwesomeIcon icon={faInstagram} size="2x" /> */}
-          <FaInstagram size="30px" />
-        </a>
-        <a
-          href="https://facebook.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            color: "white",
-          }}
-        >
-          {/* <FontAwesomeIcon icon={faFacebook} size="2x" /> */}
-          <FaFacebook size="30px" />
-        </a>
-        <button
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "white",
+              marginRight: "1rem",
+            }}
+          >
+            {/* <FontAwesomeIcon icon={faTwitter} size="2x" /> */}
+            <FaTwitter size="30px" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "white",
+              marginRight: "1rem",
+            }}
+          >
+            {/* <FontAwesomeIcon icon={faInstagram} size="2x" /> */}
+            <FaInstagram size="30px" />
+          </a>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "white",
+            }}
+          >
+            {/* <FontAwesomeIcon icon={faFacebook} size="2x" /> */}
+            <FaFacebook size="30px" />
+          </a>
+          <button
           className="btn bg-light"
           style={{
             width: "100px",
@@ -107,14 +93,10 @@ const NavigationBar = () => {
         >
           LOGOUT
         </button>
+        </div>
       </div>
-      {/* </div> */}
     </nav>
-<<<<<<< HEAD
-  
-=======
->>>>>>> 80cccb9ff46560d2fac8ecd139005fd7a9641021
   );
 };
 
-export default NavigationBar;
+export default Navbar;
