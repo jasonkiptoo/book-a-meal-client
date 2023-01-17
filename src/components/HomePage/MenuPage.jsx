f09fb76cba2771f5f26b61054c4e0ed05bfe63b1
JasonKiptoo <kiptoojason@gmail.com>
2023-01-17 13:06:14 +0300

    refactored menupage to include cards

--- src/components/HomePage/MenuPage.jsx
+++ src/components/HomePage/MenuPage.jsx
@@ -2,6 +2,7 @@ import { display } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import Navbar from "../NavBar/NavBar";
 import Specials from "../Specials/Specials";
+import MenuCard from "./MenuCard";
 
 const MyOrders = () => {
   const [menu, setMenu] = useState([]);
@@ -28,84 +29,15 @@ const MyOrders = () => {
         <Navbar />
       </div>
       {/* <div style={{ backgroundColor: "green", height: "35%", margin: "50px" }}> */}
-        <Specials />
+      <Specials />
       {/* </div> */}
-      <div
-        className="orders row row-cols-3 mt-5"
-        style={{
-          margin: "50px",
-        }}
-      >
-        {menu &&
-          menu.map((item) => (
-            <div key={item.id} className="menu-item">
-              <div
-                className="card-item"
-                style={{ backgroundColor: "transparent", border: "none" }}
-              >
-                <div
-                  className="card"
-                  style={{
-                    backgroundColor: "#D9D9D9",
-                    borderRadius: "30px",
-                    padding: "10%",
-                  }}
-                >
-                  <img
-                    src={item.image_url}
-                    alt="piktcha"
-                    className="img-fluid me-5 mt-3 mb-3"
-                    style={{
-                      height: "250px",
-                      width: "200px",
-                      borderRadius: "30px",
-                      margin: "auto",
-                    }}
-                  ></img>
-                  <p
-                    className="product-name"
-                    style={{ color: "#990F02", paddingLeft: "15%" }}
-                  >
-                    {item.name}
-                  </p>
-                  <p
-                    className="product-name"
-                    style={{ color: "#990F02", paddingLeft: "15%" }}
-                  >
-                    Ksh {item.id}
-                  </p>
-                </div>
-                <div
-                  className="card mt-3 mb-3"
-                  style={{
-                    backgroundColor: "#990F02",
-                    border: "none",
-                    borderRadius: "40px",
-                    margin: "20%",
 
-                    flexDirection: "row",
-                  }}
-                >
+        <div className="orders row row-cols-3 mt-5">
+          {menu.map((item, index) => {
+            return <MenuCard key={index} item={item} />;
+          })}
+        </div>
 
-                  <button className="btn text-white "style={{flex: "1"}}>2 Glasses</button>
-
-                  <button
-                    class="btn btn-light "
-                    id="subtract"
-                    style={{
-                      borderRadius: "40px",
-                      flex: "0.4",
-                      border: "none",
-                      color: "#990F02",
-                      fontSize: "30px",
-                    }}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
         <div
           className=" col-2 col-md-8 col-lg-12"
           style={{ paddingLeft: "40%" }}
@@ -122,7 +54,7 @@ const MyOrders = () => {
           </button>
         </div>
       </div>
-    </div>
+    
   );
 };
 
