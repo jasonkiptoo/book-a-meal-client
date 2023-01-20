a01aaaf63654d664bf61a498c5759f020c6abe40
Karen Kanana <kananakaren21@gmail.com>
2023-01-20 10:48:28 +0300

    Update file

--- src/components/HomeAdmin/NavbarAdmin.jsx
+++ src/components/HomeAdmin/NavbarAdmin.jsx
@@ -8,12 +8,13 @@ import { useCart } from "react-use-cart";
 const Navbar = ({ current }) => {
   const [activePage, setActivePage] = useState("homepage");
   let homepage = activePage === "homepage" ? "active" : "homepage";
-  let menu = activePage === "menu" ? "active" : "homepage";
-  let orders = activePage === "orders" ? "active" : "";
+  let menupage = activePage === "menupage" ? "active" : "menupage";
+  let orders = activePage === "orders" ? "active" : "orders";
   let orderhistory = activePage === "orderhistory" ? "active" : "";
 
   const [user, setUser] = useState(null);
   
+  ;
 
   useEffect(() => {
     fetch("http://127.0.0.1:3000/profile", {
@@ -54,21 +55,33 @@ const Navbar = ({ current }) => {
             padding: "20px",
           }}
         >
-          Menu
+          Home
         </Link>
-
         <Link
-          to="/my-orders"
-          className={myorders}
-          onClick={() => setActivePage("myorders")}
+          to="/menupage"
+          className={menupage}
+          onClick={() => setActivePage("menupage")}
           style={{
-            color: activePage === "myorders" ? "black" : "white",
+            color: activePage === "homepage" ? "black" : "white",
             textDecoration: "none",
             padding: "20px",
           }}
         >
-          My orders
-          <span class="badge badge-light text-dark ms-2" style={{backgroundColor: "yellow", borderRadius: "95%"}}>{totalItems}</span>
+          Menu
+        </Link>
+
+        <Link
+          to="/orders"
+          className={orders}
+          onClick={() => setActivePage("orders")}
+          style={{
+            color: activePage === "orders" ? "black" : "white",
+            textDecoration: "none",
+            padding: "20px",
+          }}
+        >
+          Orders
+          
 
 
 
@@ -107,17 +120,7 @@ const Navbar = ({ current }) => {
         }}
       >
         <div className="social-icons">
-        <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              color: "white",
-              marginRight: "1rem",
-            }}
-          >
-            <FaShoppingCart size="30px" />
-          </a>
+        
           <a
             href="https://twitter.com"
             target="_blank"
